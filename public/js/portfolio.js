3f1495439056f8f5d5778c3506edcafe639e5fda
Akhil r <akhil1842002@gmail.com>
2023-02-10 20:50:14 +0530

    uploading folder

---
+++ public/js/portfolio.js
@@ -0,0 +1,56 @@
+jQuery("#carousel").owlCarousel({
+    autoplay: true,
+    rewind: true, /* use rewind if you don't want loop */
+    margin: 20,
+    /*
+   animateOut: 'fadeOut',
+   animateIn: 'fadeIn',
+   */
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 7000,
+    smartSpeed: 800,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+  
+      600: {
+        items: 3
+      },
+  
+      1024: {
+        items: 4
+      },
+  
+      1366: {
+        items: 4
+      }
+    }
+  });
+  
+  $('#pagination-demo').twbsPagination({
+    totalPages: 35,
+    visiblePages: 7,
+    onPageClick: function (event, page) {
+      $('#page-content').text('Page ' + page);
+    }
+  });
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
